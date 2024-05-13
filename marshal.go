38d588053990be3b5bc4743a50ef519b334c1873
eslam abdulmola <eslam.infoedsx@gmail.com>
2024-05-13 11:28:19 +0200

    feat: Add support for handling bson.MongoTimestamp in Marshal and Unmarshal
    The code changes in `marshal.go` introduce support for handling `bson.MongoTimestamp` objects in the `Marshal` and `Unmarshal` functions. This allows for proper detection and handling of `$timestamp` inputs with various numeric types (`int`, `int64`, `int32`, or `float64`) on the "t" and "i" values inside. The implementation aligns with how `$date` was already being handled by `Unmarshal`.

--- marshal.go
+++ marshal.go
@@ -12,50 +12,78 @@ import (
 type Mejson map[string]interface{}
 
 func Marshal(in interface{}) (interface{}, error) {
-	// short circuit for nil
-	if in == nil {
-		return nil, nil
-	}
+    // short circuit for nil
+    if in == nil {
+        return nil, nil
+    }
 
-	if reflect.TypeOf(in).Kind() == reflect.Slice {
-		if v, ok := in.([]byte); ok {
-			return marshalBinary(bson.Binary{0x00, v}), nil
-		}
-		v := reflect.ValueOf(in)
-
-		slice := make([]interface{}, v.Len())
-		for i := 0; i < v.Len(); i++ {
-			slice[i] = v.Index(i).Interface()
-		}
-		return marshalSlice(slice)
-	} else {
-		switch v := in.(type) {
-		case bson.M:
-			return marshalMap(v)
-		case map[string]interface{}:
-			return marshalMap(v)
-		case bson.D:
-			// todo write marshaller for doc to ensure serialization order
-			return marshalMap(v.Map())
-		case bson.Binary:
-			return marshalBinary(v), nil
-		case bson.ObjectId:
+    if reflect.TypeOf(in).Kind() == reflect.Slice {
+        if v, ok := in.([]byte); ok {
+            return marshalBinary(bson.Binary{0x00, v}), nil
+        }
+        v := reflect.ValueOf(in)
+        slice := make([]interface{}, v.Len())
+        for i := 0; i < v.Len(); i++ {
+            slice[i] = v.Index(i).Interface()
+        }
+        return marshalSlice(slice)
+    } else {
+        switch v := in.(type) {
+        case primitive.M, map[string]interface{}:
+            return marshalMap(toBsonM(v))
+		case primitive.ObjectID:
 			return marshalObjectId(v), nil
-		case time.Time:
-			return marshalTime(v), nil
-		case bson.MongoTimestamp:
-			return marshalTimestamp(v), nil
-		case bson.RegEx:
-			return marshalRegex(v), nil
-		case string, int, int64, bool, float64, uint8, uint32:
-			return v, nil
-		default:
-			fmt.Fprintf(os.Stderr, "mejson: unknown type: %T\n", v)
-			return v, nil
-		}
-	}
+		case bson.D:
+            return marshalMap(v.Map())
+        case bson.Binary:
+            return marshalBinary(v), nil
+        case time.Time:
+            return marshalTime(v), nil
+        case bson.MongoTimestamp:
+            return marshalTimestamp(v), nil
+        case bson.RegEx:
+            return marshalRegex(v), nil
+		case primitive.DateTime:
+			return marshalDate(v), nil
+		case primitive.Undefined:
+            return marshalUndefined(), nil
+        case string, int, int64, bool, float64, uint8, uint32, int32:
+            // Added int32 to the existing types
+            return v, nil
+        default:
+            fmt.Fprintf(os.Stderr, "mejson: unknown !!! type: %T\n", v)
+            return v, nil
+        }
+    }
 }
 
+func toBsonM(in interface{}) bson.M {
+    switch v := in.(type) {
+    case primitive.M:
+        // Convert primitive.M to bson.M
+        bsonM := bson.M{}
+        for key, value := range v {
+            bsonM[key] = value
+        }
+        return bsonM
+    case map[string]interface{}:
+        // Directly convert map[string]interface{} to bson.M
+        return bson.M(v)
+    case bson.M:
+        // Already bson.M, return as is
+		return v
+    default:
+        // Log unexpected type and return an empty bson.M or handle the error appropriately
+        fmt.Printf("toBsonM: unexpected type %T, expected map or bson.M\n", v)
+        return bson.M{}
+    }
+}
+
+func marshalUndefined() map[string]interface{} {
+    return map[string]interface{}{
+        "$undefined": true,
+    }
+}
 func marshalSlice(in []interface{}) (interface{}, error) {
 	result := make([]interface{}, len(in))
 	for idx, value := range in {
@@ -80,8 +108,8 @@ func marshalMap(in bson.M) (map[string]interface{}, error) {
 	return result, nil
 }
 
-func marshalObjectId(in bson.ObjectId) map[string]interface{} {
-	return map[string]interface{}{"$oid": in.Hex()}
+func marshalObjectId(in primitive.ObjectID) map[string]interface{} {
+	return map[string]interface{}{"$oid": in.Hex() }
 }
 
 func marshalBinary(in bson.Binary) Mejson {
@@ -94,6 +122,12 @@ func marshalBinary(in bson.Binary) Mejson {
 func marshalTime(in time.Time) map[string]interface{} {
 	return map[string]interface{}{
 		"$date": int(in.UnixNano() / 1e6),
+	}
+}
+
+func marshalDate(in primitive.DateTime) map[string]interface{} {
+	return map[string]interface{}{
+		"$date": int64(in),
 	}
 }
 
